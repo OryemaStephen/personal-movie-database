cd794e81c138444da3f1e484698679399d327ab9
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-06-13 11:47:19 +0300

    Added responsiveness to the navigation menu

--- public/app.js
+++ public/app.js
@@ -1,16 +1,33 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const navBar = document.getElementById('navbar');
-
-    hamburger.addEventListener('click', ()=> {
-        if (hamburger.classList.contains('fa-bars')) {
-            hamburger.classList.remove('fa-bars');
-            hamburger.classList.add('fa-times');
-            navBar.style.display = 'block';
-        } else {
+    //Navigation menu
+    //Display or hide nav items and hamburger
+    function toggleNavMenu(){
+        if (navBar.style.display === 'block') {
+            navBar.style.display = 'none';
             hamburger.classList.remove('fa-times');
             hamburger.classList.add('fa-bars');
-            navBar.style.display = 'none';
+        } else {
+            navBar.style.display = 'block';
+            hamburger.classList.remove('fa-bars');
+            hamburger.classList.add('fa-times');
         }
-    });
-})
+    };
+
+    hamburger.addEventListener('click', toggleNavMenu);
+
+    // Initial Nav state
+    function initialNavState(){
+        if (window.innerWidth <= 768) {
+            navBar.style.display = 'none';
+            hamburger.classList.add('fa-bars');
+        } else {
+            navBar.style.display = 'flex';
+            hamburger.classList.remove('fa-bars', 'fa-times');
+        }
+    };
+
+    initialNavState();
+    window.addEventListener('resize', initialNavState);
+});
