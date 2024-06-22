21ac35a3efe93347dbfaa526195a4cc5f12cae50
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-06-22 10:57:55 +0300

    Added cors

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
